e014254155de75d9acbdf05e5d09338a37d01c3b
Tushpendra Kumar <your_github_email@example.com>
2025-09-26 12:45:39 +0530

    Updated my code

--- main.js
+++ main.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Web Developer","Fresher","Technician", "Coder", "Editer"],
+        strings: ["Learn Coding", "Watch Tutorials", "Explore Projects", "Code for Free"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
@@ -48,7 +48,7 @@ $(document).ready(function(){
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Web Developer","Fresher" ,"Technician", "Coder", "Editer"],
+        strings: ["Innovators", "Developers", "Creators", "Problem Solvers", "Visionaries", "Tech Enthusiasts"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
