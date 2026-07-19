c4eeb560691f3fca65fad42e9286d6df69b5070a
tushu <ritiksingh7055072196>
2026-07-19 23:50:50 +0530

    feat: simplify mobile navbar to show only cart icon on courses page

--- courses/index.php
+++ courses/index.php
@@ -39,7 +39,7 @@ if (Auth::check()) {
     <!-- Main Site Styles -->
     <link rel="stylesheet" href="../styles.css?v=40">
     <!-- Courses Styles -->
-    <link rel="stylesheet" href="./courses.css?v=3">
+    <link rel="stylesheet" href="./courses.css?v=4">
 
     <style>
         .material-symbols-rounded {
