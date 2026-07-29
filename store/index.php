b5d3c7bc798ce648eaaf63f774862f31509577f1
tushu <ritiksingh7055072196>
2026-07-30 03:45:18 +0530

    Bust browser cache for store.css to apply testimonial layout fixes

--- store/index.php
+++ store/index.php
@@ -19,7 +19,7 @@ Auth::requireLogin();
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" crossorigin="anonymous" referrerpolicy="no-referrer">
     
     <link rel="stylesheet" href="../styles.css?v=40">
-    <link rel="stylesheet" href="./store.css?v=2">
+    <link rel="stylesheet" href="./store.css?v=3">
 </head>
 <body>
     <!-- Futuristic Animated Background (Global for Store) -->
